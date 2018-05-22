3f406dd6fe6af9d620258102a8c2aefcf6130aad
Sam Gleske <sam.mxracer@gmail.com>
2018-05-22 02:16:58 -0700

    Try to build babl

--- vars/gimpBuildPipeline.groovy
+++ vars/gimpBuildPipeline.groovy
@@ -12,24 +12,28 @@
 projectEnvMap = [
     gimp: [
         master: [
-            BABL_BRANCH: master,
-            GEGL_BRANCH: master
+            GEGL_BRANCH: 'master'
         ],
         'gimp-2-10': [
-            BABL_BRANCH
+            GEGL_BRANCH: 'master'
+        ],
+        'gimp-2-8': [
+            GEGL_BRANCH: 'gegl-0-2'
         ]
     ]
 ]
 
-/*
 String getDockerEnv(String project, branch) {
+    projectEnvMap[project]?.get(branch)?.collect { k, v ->
+        "-e ${k}=${v}"
+    }.join(' ') ?: ''
 }
-*/
 
 def call() {
-    //e.g. project = babl
+    //e.g. project = gimp
     String project = env.JOB_NAME.tokenize('/')[0]
-    String myEnv = "-e ${project.toUpperCase()}_BRANCH=${env.BRANCH_NAME}"
+    //e.g. -e GIMP_BRANCH=master
+    String myEnv = "-e ${project.toUpperCase()}_BRANCH=${env.BRANCH_NAME}" + getDockerEnv(project, env.BRANCH_NAME)
     node('master') {
         stage("Environment") {
             docker.image('gimp/gimp:latest').inside("${myEnv} -v gimp-git-data:/export:ro") {
@@ -38,6 +42,14 @@ def call() {
             }
         }
         stage("Build ${project}") {
+            docker.image('gimp/gimp:latest').inside("${myEnv} -v gimp-git-data:/export:ro") {
+                //automatically generated checkout command from pipeline syntax generator
+                checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'refs/heads/master']], browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/gimp-ci/docker-jenkins-gimp'], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'ChangelogToBranch', options: [compareRemote: 'origin', compareTarget: 'master']], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'docker-jenkins-gimp']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/gimp-ci/docker-jenkins-gimp']]]
+                //end automatically generated checkout
+
+                sh "bash ./docker-jenkins-gimp/debian-testing/${project}.sh"
+                archiveArtifacts artifacts: "${project}/${project}-internal.tar.gz", fingerprint: true, onlyIfSuccessful: true
+            }
         }
     }
 }
