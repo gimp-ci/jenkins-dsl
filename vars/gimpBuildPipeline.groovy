32743578e5954234ef44ad77a7160c9c64638371
Sam Gleske <sam.mxracer@gmail.com>
2018-05-22 03:48:18 -0700

    Support copying dependencies from other pipelines

--- vars/gimpBuildPipeline.groovy
+++ vars/gimpBuildPipeline.groovy
@@ -30,6 +30,53 @@ Map projectEnvMap() {
     ]
 }
 
+/**
+  List of projects and branches in which to copy dependencies from.
+ */
+@NonCPS
+List<String> projectDependencies(String project, String branch) {
+    [
+        gegl: [
+            master: [
+                '../babl/master'
+            ]
+        ],
+        libmypaint: [
+            'v1.3.0': [
+                '../babl/master',
+                '../gegl/master'
+            ]
+        ],
+        'mypaint-brushes': [
+            'v0.3.x': [
+                '../babl/master',
+                '../gegl/master',
+                '../libmypaint/v1.3.0'
+            ]
+        ],
+        gimp: [
+            'master': [
+                '../babl/master',
+                '../gegl/master',
+                '../libmypaint/v1.3.0',
+                '../mypaint-brushes/v0.3.x'
+            ],
+            'gimp-2-10': [
+                '../babl/master',
+                '../gegl/master',
+                '../libmypaint/v1.3.0',
+                '../mypaint-brushes/v0.3.x'
+            ],
+            'gimp-2-8': [
+                '../babl/master',
+                '../gegl/gegl-0-2',
+                '../libmypaint/v1.3.0',
+                '../mypaint-brushes/v0.3.x'
+            ]
+        ]
+    ].get(project)?.get(branch) ?: []
+}
+
 @NonCPS
 String getFriendlyName(String name) {
     [
@@ -50,10 +97,13 @@ String getDockerEnv(String project, branch) {
     }?.join(' ') ?: ''
 }
 
+/**
+  This is the main execution.
+ */
 def call() {
     //e.g. project = gimp
     String project = env.JOB_NAME.tokenize('/')[0]
-    //e.g. -e GIMP_BRANCH=master
+    //e.g. -e GIMP_BRANCH=master -e GEGL_BRANCH=master
     String myEnv = "-e ${project.toUpperCase()}_BRANCH=${env.BRANCH_NAME}" + getDockerEnv(project, env.BRANCH_NAME)
     node('master') {
         stage("Environment") {
@@ -63,6 +113,12 @@ def call() {
             }
         }
         docker.image('gimp/gimp:latest').inside("${myEnv} -v gimp-git-data:/export:ro") {
+            stage('Copy Dependencies') {
+                for(String dependency : projectDependencies(project, env.BRANCH_NAME)) {
+                    copyArtifacts fingerprintArtifacts: true, flatten: true, projectName: '../foo/bar', selector: lastSuccessful()
+                    sh 'mv *.tar.gz /data'
+                }
+            }
             stage("Build ${getFriendlyName(project)}") {
                 //automatically generated checkout command from pipeline syntax generator
                 checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'refs/heads/master']], browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/gimp-ci/docker-jenkins-gimp'], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'ChangelogToBranch', options: [compareRemote: 'origin', compareTarget: 'master']], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'docker-jenkins-gimp']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/gimp-ci/docker-jenkins-gimp']]]
