b9e2aefd732dd7ef64328235f6244f61dc1a28b7
Sam Gleske <sam.mxracer@gmail.com>
2018-05-22 00:43:58 -0700

    First pipeline DSL

---
+++ vars/gimpBuildPipeline.groovy
@@ -0,0 +1,18 @@
+/*
+ * This build pipeline is designed to automatically run builds for the following projects:
+ *
+ *   - BABL
+ *   - GEGL
+ *   - libmypaint
+ *   - mypaint-brushes
+ *   - GIMP
+ *
+ * The pipeline will select appropriate build scripts depending on the project calling it.
+ */
+
+node('docker-host') {
+    stage("Environment") {
+        environment_string = sh(script: 'env | LC_ALL=C sort', returnStdout: true).split('\n').join('\n    ')
+        echo "ENVIRONMENT:\n    ${environment_string}"
+    }
+}
