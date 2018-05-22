c207a1f2f581c418220adc285e833ac7882a3298
Sam Gleske <sam.mxracer@gmail.com>
2018-05-22 01:34:02 -0700

    Mention docker environment

--- vars/gimpBuildPipeline.groovy
+++ vars/gimpBuildPipeline.groovy
@@ -15,7 +15,7 @@ def call() {
         stage("Environment") {
             docker.image('gimp/gimp:latest').inside("-v gimp-git-data:/export:ro") {
                 environment_string = sh(script: 'env | LC_ALL=C sort | grep -E \'BRANCH|^BUILD_|^JOB_|PATH|^DEBIAN_FRONTEND|^PREFIX|ACLOCAL_FLAGS|^PWD\'', returnStdout: true).split('\n').join('\n    ')
-                echo "ENVIRONMENT:\n    ${environment_string}"
+                echo "DOCKER ENVIRONMENT:\n    ${environment_string}"
             }
         }
     }
