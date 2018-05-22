90d821c56a03daf5e60feb35f12f1b75ef8fe77e
Sam Gleske <sam.mxracer@gmail.com>
2018-05-22 02:32:23 -0700

    clean workspace after run

--- vars/gimpBuildPipeline.groovy
+++ vars/gimpBuildPipeline.groovy
@@ -74,5 +74,6 @@ def call() {
                 archiveArtifacts artifacts: "${project}/${project}-internal.tar.gz", fingerprint: true, onlyIfSuccessful: true
             }
         }
+        deleteDir()
     }
 }
